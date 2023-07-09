a913579e56318ebaf6bb640105b9ee9f58a9c15f
Agung Saputra <agungsaputradifh@gmail.com>
2023-07-09 22:30:11 +0700

    dark mode v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,11 @@ module.exports = {
     extend: {
       colors: {
         primary: "#5A96E3",
-        
-        dark: "#0f172a",
+        dark: "#16213E",
+        dark2 : "#0F3460",
+        dark3: "#533483",
+        darkPrimary : "#E94560",
+        darkHover : "#653a5c"
       },
       screens: {
         "2xl": "1320px",
